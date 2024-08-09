a44034931c842805bfae2ee55b39bc85d667e06d
Vivek Reddy <vivekreddy@Viveks-MacBook-Pro-2.local>
2024-08-09 16:36:16 +0530

    basic HTML

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+@app.route('/meditation')
+def meditation():
+    return 'Meditation Page!'
+
+@app.route('/workouts')
+def workouts():
+    return 'Workouts Page!'
+
+@app.route('/nutrition')
+def nutrition():
+    return 'Nutrition Page!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
